03931ecc683fbc33ecd568757bb9fdcabaa8dca3
Saeed Ahmed <saeedahmed9326@gmail.com>
2024-05-14 12:43:06 +0500

    create variable - var in typescript

---
+++ dist/Variable-var.js
@@ -0,0 +1,30 @@
+"use strict";
+//Murk Kitab Ghar Karachi. 
+var TotalAmount = 0;
+console.log("............");
+var BookOne = 200; //Declare 
+BookOne = 300; //Reassign
+var BookOne = 600; //Re-declare
+var BookTwo = 400;
+var BookThree = 200;
+var BookFour = 1000;
+var BookFive = 600;
+var BookSix = 700;
+var BookSeven = 200;
+var BookEight = 700;
+var BookNine = 500;
+var BookTen = 400;
+console.log("Faliure is the first step of sucess (Motivation) ", BookOne);
+console.log("Bequest of Bhattai (Latifiyat) ", BookTwo);
+console.log("I Love you (Novel) ", BookThree);
+console.log("Shah Jo Risalo (Translation)", BookFour);
+console.log("Sindh Graduates Association 50 year of Service to Sindh (History) ", BookFive);
+console.log("Utopia to dystopia (Literary criticism) ", BookSix);
+console.log("Sindhi basic grammar ", BookSeven);
+console.log("Shah Latif (Novel) ", BookEight);
+console.log("Tankid nigari aein sindhi adab (Criticism)", BookNine);
+console.log("Samaj aein siyast jo graimar (Criticism)", BookTen);
+console.log("............");
+TotalAmount = BookOne + BookTwo + BookThree + BookFour + BookFive + BookSix + BookSeven + BookEight + BookNine + BookTen;
+console.log("Your total bill is = ", TotalAmount);
+console.log("............");
---
+++ src/Variable-var.ts
@@ -0,0 +1,31 @@
+//Murk Kitab Ghar Karachi. 
+var TotalAmount: number = 0;
+console.log("............"); 
+var BookOne         : any = 200; //Declare 
+BookOne                   = 300; //Reassign
+var BookOne         : any = 600; //Re-declare
+var BookTwo         : any = 400;
+var BookThree       : any = 200;
+var BookFour        : any = 1000;
+var BookFive        : any = 600;
+var BookSix         : any = 700;
+var BookSeven       : any = 200;
+var BookEight       : any = 700;
+var BookNine        : any = 500;
+var BookTen         : any = 400;
+console.log("Faliure is the first step of sucess (Motivation) ", BookOne);
+console.log("Bequest of Bhattai (Latifiyat) ", BookTwo);
+console.log("I Love you (Novel) ", BookThree);
+console.log("Shah Jo Risalo (Translation)", BookFour);
+console.log("Sindh Graduates Association 50 year of Service to Sindh (History) ", BookFive);
+console.log("Utopia to dystopia (Literary criticism) ", BookSix);
+console.log("Sindhi basic grammar ",BookSeven);
+console.log("Shah Latif (Novel) ", BookEight);
+console.log("Tankid nigari aein sindhi adab (Criticism)", BookNine);
+console.log("Samaj aein siyast jo graimar (Criticism)", BookTen);
+
+console.log("............"); 
+
+TotalAmount = BookOne + BookTwo + BookThree + BookFour + BookFive + BookSix + BookSeven + BookEight + BookNine + BookTen 
+console.log("Your total bill is = ",TotalAmount); 
+console.log("............"); 
